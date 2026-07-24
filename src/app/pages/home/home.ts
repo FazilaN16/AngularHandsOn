fa7ecda97d746b78c810d286d313f065e497533e
Fazila <fazila.nms@gmail.com>
2026-07-24 15:42:54 +0530

    Complete Angular Hands-on 2

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit, OnDestroy {
+
+  portalName = 'Student Course Portal';
+  isPortalActive = true;
+  message = '';
+  searchTerm = '';
+  courseCount = 0;
+
+  onEnrollClick() {
+    this.message = 'Enrollment opened!';
+  }
+
+  ngOnInit(): void {
+    this.courseCount = 12;
+    console.log('HomeComponent initialised — courses loaded');
+  }
+
+  ngOnDestroy(): void {
+  console.log('HomeComponent destroyed');
+}
+}
+
+// [property] is one-way binding (Component → DOM)
+// [(ngModel)] is two-way binding (Component ↔ DOM)
